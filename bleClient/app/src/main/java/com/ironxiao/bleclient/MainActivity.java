451d10fdeab21878dfcef79f2224e84fc085f9b3
IronXiao <xiaoleigs@gmail.com>
2020-07-09 18:40:18 +0800

    first init

---
+++ bleClient/app/src/main/java/com/ironxiao/bleclient/MainActivity.java
@@ -0,0 +1,261 @@
+package com.ironxiao.bleclient;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "XL_BLE_CLIENT";
+
+    private static final int REQUEST_ENABLE_BT = 0x0;
+    private static final int REQUEST_PERMISSIONS_ACCESS_LOCATION = 0x1;
+
+    private static final int MANUFACTURE_ID = 0xAAA;
+    private static final String MANUFACTURE = "Nologic";
+
+    private static final UUID UUID_SETUP = UUID.fromString("00001ff9-0000-1000-8000-00805f9b34fb");
+    private static final UUID UUID_REQUEST = UUID.fromString("00001ffa-0000-1000-8000-00805f9b34fb");
+
+    private ScanResult scanResultToConnect = null;
+
+    private BluetoothAdapter bluetoothAdapter;
+
+    private BluetoothGattCharacteristic targetCharacteristic = null;
+
+    private BluetoothGatt targetBluetoothGatt = null;
+
+    private final ScanCallback leScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            log("onScanResult: " + callbackType + ", result: " + result + ", device: " + result.getDevice().getName());
+            //target device founded,cancel scan to save battery!
+            scanResultToConnect = result;
+            scanLeDeviceCommon(false);
+            connectBleDevice(scanResultToConnect);
+        }
+
+    };
+
+    private static final ArrayList<ScanFilter> scanFilters = new ArrayList<>();
+
+    static {
+        scanFilters.add(new ScanFilter.Builder().setServiceUuid(new ParcelUuid(UUID_SETUP)).setManufacturerData(MANUFACTURE_ID, MANUFACTURE.getBytes()).build());
+    }
+
+    private static final ScanSettings scanSettings = new ScanSettings.Builder().setNumOfMatches(ScanSettings.MATCH_NUM_ONE_ADVERTISEMENT).setScanMode(ScanSettings.SCAN_MODE_LOW_POWER).build();
+
+    private final BluetoothGattCallback bluetoothGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+            log("onConnectionStateChange:" + gatt.getDevice() + ", status: " + status + ", newState: " + newState);
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                log("start to discover services!");
+                gatt.discoverServices();
+            } else {
+                log("lose connection!");
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+            log("onServicesDiscovered:" + gatt.getDevice() + ", status: " + status);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                gatt.connect();
+                List<BluetoothGattService> services = gatt.getServices();
+                log("read services !");
+                for (BluetoothGattService service : services) {
+                    log("found " + service.toString());
+                    if (service.getUuid().equals(UUID_SETUP)) {
+                        List<BluetoothGattCharacteristic> cList = service.getCharacteristics();
+                        for (BluetoothGattCharacteristic bgc : cList) {
+                            log(bgc.getUuid().toString());
+                            if (bgc.getUuid().equals(UUID_REQUEST)) {
+                                log("found target service !");
+                                //gatt.setCharacteristicNotification(bgc, true);
+                                targetCharacteristic = bgc;
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (!hasBleFeature()) {
+            toast("This device does not support ble!!!");
+        }
+
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+    }
+
+    public void startBleSan(View view) {
+        if (!hasBleFeature()) {
+            toast("This device does not support ble!!!");
+            return;
+        }
+        if (!btEnabled()) {
+            toast("BT not opened, please enable BT first !");
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            return;
+        }
+        if (((Button) view).getText().equals(getString(R.string.start_ble_scan))) {
+            scanLeDeviceCommon(true);
+        } else {
+            scanLeDeviceCommon(false);
+        }
+    }
+
+    private void scanLeDeviceCommon(final boolean enable) {
+        if (enable) {
+            if (!accessLocationAllowed()) {
+                toast("Pls grant location permission first, otherwise cannot search device!");
+                grantAccessLocationPermission();
+                return;
+            }
+            log("Start LeScan !");
+            ((Button) findViewById(R.id.scan)).setText(R.string.stop_ble_scan);
+            bluetoothAdapter.getBluetoothLeScanner().startScan(scanFilters, scanSettings, leScanCallback);
+        } else {
+            ((Button) findViewById(R.id.scan)).setText(R.string.start_ble_scan);
+            bluetoothAdapter.getBluetoothLeScanner().stopScan(leScanCallback);
+            log("Stop LeScan !");
+        }
+    }
+
+    private boolean btEnabled() {
+        return bluetoothAdapter == null || bluetoothAdapter.isEnabled();
+    }
+
+    private boolean hasBleFeature() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE);
+    }
+
+    private void toast(String showStr) {
+        Toast.makeText(MainActivity.this, showStr, Toast.LENGTH_SHORT).show();
+    }
+
+    private static void log(String log) {
+        Log.d(TAG, log);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (btEnabled())
+                toast("BT enabled success !");
+            else
+                toast("BT enabled fail !");
+        }
+    }
+
+    private boolean accessLocationAllowed() {
+        if (!osVersionLaterThanP()) {
+            return ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED;
+
+        }
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
+
+    }
+
+    private boolean osVersionLaterThanP() {
+        return Build.VERSION.SDK_INT > Build.VERSION_CODES.P;
+    }
+
+    private void grantAccessLocationPermission() {
+        if (accessLocationAllowed()) return;
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            //TODO further action
+        } else {
+            if (!osVersionLaterThanP())
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_PERMISSIONS_ACCESS_LOCATION);
+            else
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_PERMISSIONS_ACCESS_LOCATION);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSIONS_ACCESS_LOCATION) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                toast("location permission granted!");
+            } else {
+                toast("Can not scan device without permission!");
+            }
+        }
+    }
+
+    private void connectBleDevice(ScanResult scanResultToConnect) {
+        if (scanResultToConnect != null)
+            targetBluetoothGatt = scanResultToConnect.getDevice().connectGatt(MainActivity.this, false, bluetoothGattCallback);
+    }
+
+
+    public void sendMsg(View view) {
+        if (targetBluetoothGatt == null || targetCharacteristic == null) {
+            return;
+        } else {
+            targetBluetoothGatt.connect();
+            targetCharacteristic.setValue("test");
+            targetBluetoothGatt.writeCharacteristic(targetCharacteristic);
+        }
+    }
+}
---
+++ bleServer/app/src/main/java/com/ironxiao/bleserver/MainActivity.java
@@ -0,0 +1,228 @@
+package com.ironxiao.bleserver;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattServer;
+import android.bluetooth.BluetoothGattServerCallback;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+import static android.bluetooth.BluetoothGattCharacteristic.PERMISSION_READ;
+import static android.bluetooth.BluetoothGattCharacteristic.PERMISSION_WRITE;
+import static android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ;
+import static android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE;
+//import static android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE;
+import static android.bluetooth.BluetoothGattService.SERVICE_TYPE_PRIMARY;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "XL_BLE_SERVER";
+
+    //We are Nologic Inc.
+    private static final int MANUFACTURE_ID = 0xAAA;
+    private static final String MANUFACTURE = "Nologic";
+
+
+    private static final int REQUEST_ENABLE_BT = 0x0;
+    private static final UUID UUID_SETUP = UUID.fromString("00001ff9-0000-1000-8000-00805f9b34fb");
+    private static final UUID UUID_REQUEST = UUID.fromString("00001ffa-0000-1000-8000-00805f9b34fb");
+
+
+    private BluetoothManager bluetoothManager;
+    private BluetoothAdapter bluetoothAdapter;
+
+    private BluetoothGattServer bluetoothGattServer;
+    private BluetoothLeAdvertiser bluetoothLeAdvertiser;
+    private static final BluetoothGattService bluetoothGattService = new BluetoothGattService(UUID_SETUP, SERVICE_TYPE_PRIMARY);
+
+    static {
+        bluetoothGattService.addCharacteristic(new BluetoothGattCharacteristic(UUID_REQUEST,
+                PROPERTY_WRITE | PROPERTY_READ,
+                PERMISSION_READ | PERMISSION_WRITE));
+    }
+
+
+    private final BluetoothGattServerCallback bluetoothGattServerCallback = new BluetoothGattServerCallback() {
+
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
+            super.onConnectionStateChange(device, status, newState);
+            log("onConnectionStateChange:" + device.getName() + ", status: " + status + ", newState: " + newState);
+            toast("onConnectionStateChange" + device.getName() + ", newState:" + newState);
+        }
+
+        @Override
+        public void onCharacteristicReadRequest(BluetoothDevice device,
+                                                int requestId,
+                                                int offset,
+                                                BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicReadRequest(device, requestId, offset, characteristic);
+            log("onCharacteristicReadRequest: " + device.getName() + ", requestId: " + requestId + ", offset: " + offset + ", characteristic: " + characteristic);
+        }
+
+        @Override
+        public void onCharacteristicWriteRequest(BluetoothDevice device,
+                                                 int requestId,
+                                                 BluetoothGattCharacteristic characteristic,
+                                                 boolean preparedWrite,
+                                                 boolean responseNeeded,
+                                                 int offset,
+                                                 byte[] value) {
+            log("onCharacteristicWriteRequest: " + "requestId: " + requestId + ", characteristic: " + characteristic + ", preparedWrite: " + preparedWrite + ", responseNeeded: " + responseNeeded + ", offset: " + offset + ", value: " + Arrays.toString(value));
+            toast("onCharacteristicWriteRequest: " + new String(value));
+        }
+    };
+
+    private final AdvertiseCallback advertiseCallback = new AdvertiseCallback() {
+        @Override
+        public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+            super.onStartSuccess(settingsInEffect);
+            toast("Start ble ad success !");
+        }
+
+        @Override
+        public void onStartFailure(int errorCode) {
+            super.onStartFailure(errorCode);
+            toast("Start ble ad fail !");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+    }
+
+    public void startBleAd(View view) {
+        //Check has feature ble
+        if (!hasBleFeature()) {
+            toast("BLE not supported !");
+            return;
+        }
+        //Check bluetooth enabled
+        if (!btEnabled()) {
+            toast("BT not opened,please enable BT first !");
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            return;
+        }
+
+        if (getResources().getString(R.string.start_server).contentEquals(((Button) view).getText())) {
+            startBleAd();
+            ((Button) view).setText(R.string.stop_server);
+        } else {
+            stopBleAd(false);
+            ((Button) view).setText(R.string.start_server);
+        }
+
+
+    }
+
+    private AdvertiseSettings getAdvertiseSettings() {
+        AdvertiseSettings.Builder settingsBuilder = new AdvertiseSettings.Builder();
+        settingsBuilder.setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY);
+        settingsBuilder.setConnectable(true);
+        settingsBuilder.setTimeout(0);
+        settingsBuilder.setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH);
+        return settingsBuilder.build();
+    }
+
+    private AdvertiseData getAdvertiseData() {
+        AdvertiseData.Builder dataBuilder = new AdvertiseData.Builder();
+        dataBuilder.setIncludeDeviceName(true);
+        dataBuilder.setIncludeTxPowerLevel(true);
+        dataBuilder.addManufacturerData(MANUFACTURE_ID, MANUFACTURE.getBytes());
+        dataBuilder.addServiceUuid(new ParcelUuid(UUID_SETUP));
+        return dataBuilder.build();
+    }
+
+
+    private boolean btEnabled() {
+        return bluetoothAdapter == null || bluetoothAdapter.isEnabled();
+    }
+
+    private boolean hasBleFeature() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopBleAd(true);
+    }
+
+    private void stopBleAd(boolean destroy) {
+        if (bluetoothGattServer != null) {
+            bluetoothGattServer.clearServices();
+            bluetoothGattServer.close();
+            if (destroy)
+                bluetoothGattServer = null;
+        }
+
+        if (bluetoothLeAdvertiser != null) {
+            bluetoothLeAdvertiser.stopAdvertising(advertiseCallback);
+            if (destroy)
+                bluetoothLeAdvertiser = null;
+        }
+    }
+
+    private void startBleAd() {
+        //Start GattServer for other's command
+        if (bluetoothGattServer == null)
+            bluetoothGattServer = bluetoothManager.openGattServer(this, bluetoothGattServerCallback);
+        bluetoothGattServer.addService(bluetoothGattService);
+
+        //Start BLE Advertise for other's search
+        if (bluetoothLeAdvertiser == null)
+            bluetoothLeAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
+        bluetoothLeAdvertiser.startAdvertising(getAdvertiseSettings(), getAdvertiseData(), advertiseCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (btEnabled())
+                toast("BT enabled success !");
+            else
+                toast("BT enabled fail !");
+        }
+    }
+
+
+    private void toast(final String showStr) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, showStr, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void log(String log) {
+        Log.d(TAG, log);
+    }
+
+}
